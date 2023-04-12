ff52026a1a1b42245088e9c3c847006fecc16a8f
tanvir1985 <tanvir.hossain1985@gmail.com>
2023-04-12 01:41:06 -0600

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return list of requirements
+    '''
+    requirements = []
+    with open (file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","")for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+name = 'ML_END_TO_END',
+version = '0.0.1',
+author = "Tanvir",
+author_email= "tanvir.hossain1985@gmail.com",
+packages = find_packages(),
+install_requires = get_requirements('requirements.txt')
+
+)
